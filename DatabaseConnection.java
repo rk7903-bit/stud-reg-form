96901175f378412ca32a57c0965e48fe843ebaf0
rk7903-bit <rk7903@srmist.edu.in>
2025-10-24 12:17:26 +0530

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
